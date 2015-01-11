cfd2f39db96404d9b2bdf14021063e90e24b9771
VOID001 <zhangjianqiu13@gmail.com>
2015-01-11 18:13:23 +0800

    Add part of common headers and Status enum type

--- comm.h
+++ comm.h
@@ -1,4 +1,16 @@
 #ifndef COMM_H
 #define COMM_H
+#include<QtGui>
+
+
+//Define enumeration type Status
+//Meaning of each State
+/*
+ OK: return Normally
+ OF overflow issue occured
+ NOTFUND return this when a node cannot be found in tree
+ TREECONSTRUCT_ERR the tree must be built wrong
+  */
+typedef enum{OK,OF,NOTFOUND,TREECONSTRUCT_ERR} Status;
 
 #endif // COMM_H

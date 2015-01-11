71d85d1b0ba9120d0e42528f6d6aadf232836042
VOID001 <zhangjianqiu13@gmail.com>
2015-01-11 18:12:31 +0800

    Create Class RBNode

--- rbnode.h
+++ rbnode.h
@@ -1,4 +1,25 @@
 #ifndef RBNODE_H
 #define RBNODE_H
 
+class RBNode
+{
+private:
+	int key;
+	int bh;
+	int color;				//0 is black 1 is red
+	RBNode* parent;
+	RBNode* left,right;
+public:
+	RBNode(const int& key);
+	int bh() const;
+	int key() const;
+	int color() const;
+	RBNode* left() const;
+	RBNode* right() const;
+	RBNode* parent() const;
+	void setLeft(const RBNode* node);
+	void setRight(const RBNode* node);
+	void setParent(const RBNode* node);
+};
+
 #endif // RBNODE_H

1ba657df39d79d013462fcfd34423d7ae66515c3
VOID001 <zhangjianqiu13@gmail.com>
2015-01-11 18:11:32 +0800

    Add the Rotate functions

--- rbtree.cpp
+++ rbtree.cpp
@@ -1,1 +1,83 @@
+#include"rbtree.h"
 
+Status RBTree::L_Rotate(RBNode *node)
+{
+	if(!isNil(node->right))
+	{
+		RBNode* rnode=node->right();
+		node->setRight(rnode->left());
+		if(!isNil(rnode->left))
+		{
+			rnode->left()->setParent();
+		}
+		rnode->setParent(node->parent());
+		if(isNil(node->parent()));
+		{
+			root=rnode;
+		}
+		else if(node->parent()->left()==node)
+		{
+			node->parent()->setLeft(rnode);
+		}
+		else 
+		{
+			node->parent->setRight(rnode);
+		}
+		rnode->setLeft(node);
+		node->setParent(rnode);
+		return OK;
+	}
+	else
+	{
+		return TREECONSTRUCT_ERR;
+	}
+}
+
+Status R_Rotate(RBNode* node)
+{
+	if(!isNil(node->left))
+	{
+		RBNode* lnode=node->left();
+		node->setLeft(lnode->right());
+		if(!isNil(lnode->right()))
+		{
+			lnode->right()->setParent(node);
+		}
+		lnode->setParent(node->parent());
+		if(isNil(node->parent))
+		{
+			root=lnode;
+		}
+		else if(node->parent()->right()==node)
+		{
+			node->parent()->setRight(lnode);
+		}
+		else
+		{
+			node->parent()->setLeft(lnode);
+		}
+		lnode->setRight(node);
+		node->setParent(lnode);
+		return OK;
+	}
+	else
+	{
+		return TREECONSTRUCT_ERR;
+	}
+}
+
+RBTree::RBTree()
+{
+	root=new RBNode;
+	nil=new RBNode;
+	root.setLeft(nil);
+	root.setRight(nil);
+	nil.setLeft(root);
+	nil.setRight(root);
+}
+
+
+RBTree::~RBTree()
+{
+
+}
--- rbtree.h
+++ rbtree.h
@@ -1,4 +1,26 @@
 #ifndef RBTREE_H
 #define RBTREE_H
+#include "comm.h"
+#include "rbnode.h"
+
+class RBTree
+{
+private:
+	RBNode* root;
+	RBNode* nil;					//the pivot which has a useless value
+	Status L_Rotate(RBNode* node);
+	Status R_Rotate(RBNode* node);
+	bool isNil(RBNode* node);
+public:
+	Status addNode(const int& key);
+	Status delNode(const int& key);
+	Status find(const int& key);
+	Status createTree(QVector<RBNode*> nodeContainer);
+	
+	//RBTree(QVector<RBNode*> nodeContanier);
+	RBTree();
+	~RBTree();
+	
+};
 
 #endif // RBTREE_H

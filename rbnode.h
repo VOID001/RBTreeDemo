3489f99e78068ffbe282e41927db16a47b716289
VOID001 <zhangjianqiu13@gmail.com>
2015-01-12 00:25:46 +0800

    Add QGraphic Classes to RBNode Class

--- rbnode.h
+++ rbnode.h
@@ -1,8 +1,9 @@
 #ifndef RBNODE_H
 #define RBNODE_H
 #include"comm.h"
+#include"rblink.h"
 
-class RBNode
+class RBNode:public QGraphicsItem
 {
 private:
 	int nKey;
@@ -10,6 +11,13 @@ private:
 	NodeColor nColor;
 	RBNode* nParent;
 	RBNode* nLeft,*nRight;
+	
+	QColor nbgColor;
+	QColor ntextColor;
+	QString nText;
+	QSet<Link*> nodeLinks;
+	QRectF outlineRect() const;
+	int roundness(double size) const;
 public:
 	RBNode(const int& key);
 	RBNode();
@@ -24,7 +32,10 @@ public:
 	void setParent(RBNode* node);
 	void setKey(const int& key);
 	void setColor(const NodeColor& color);
-	
+
+	void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
+	void addLink(RBLink* link);
+	void removeLink(RBLink* link);
 	//operator Overload But now maybe not useful
 	//bool operator < (const RBNode& a) const
 	//{
@@ -35,6 +46,11 @@ public:
 	//bool operator >= (const RBNode& a) const {return !(a<(*this)); }
 	//bool operator == (const RBNode& a) const {return !(a>*this && *this>a); }
 	//bool operator !=(const RBNode& a) const  {return !(a==(*this));}
+protected:
+	QVariant itemChange(GraphicsItemChange change, const QVariant &value);
+	void mouseMoveEvent(QGraphicsSceneMouseEvent* event);
+	
+	
 };
 
 #endif // RBNODE_H

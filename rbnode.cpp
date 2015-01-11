51e947d476b003ba825224007aadf90e5d655055
VOID001 <zhangjianqiu13@gmail.com>
2015-01-11 19:36:29 +0800

    Add Basic functions to Class RBNode and fix some grammar mistakes

--- rbnode.cpp
+++ rbnode.cpp
@@ -1,1 +1,66 @@
+#include"rbnode.h"
 
+RBNode::RBNode()
+{
+
+}
+
+RBNode::RBNode(const int& key):nKey(key)
+{
+	
+}
+
+int RBNode::bh() const
+{
+	return nBh;
+}
+
+int RBNode::key() const
+{
+	return nKey;
+}
+
+NodeColor RBNode::color() const
+{
+	return nColor;
+}
+
+RBNode* RBNode::left() const
+{
+	return nLeft;
+}
+
+RBNode* RBNode::right() const
+{
+	return nRight;
+}
+
+RBNode* RBNode::parent() const
+{
+	return nParent;
+}
+
+void RBNode::setColor(const NodeColor &color)
+{
+	nColor=color;
+}
+
+void RBNode::setKey(const int &key)
+{
+	nKey=key;
+}
+
+void RBNode::setLeft(RBNode *node)
+{
+	nLeft=node;
+}
+
+void RBNode::setRight(RBNode *node)
+{
+	nRight=node;
+}
+
+void RBNode::setParent(RBNode *node)
+{
+	nParent=node;
+}
--- rbnode.h
+++ rbnode.h
@@ -1,25 +1,40 @@
 #ifndef RBNODE_H
 #define RBNODE_H
+#include"comm.h"
 
 class RBNode
 {
 private:
-	int key;
-	int bh;
-	int color;				//0 is black 1 is red
-	RBNode* parent;
-	RBNode* left,right;
+	int nKey;
+	int nBh;
+	NodeColor nColor;
+	RBNode* nParent;
+	RBNode* nLeft,*nRight;
 public:
 	RBNode(const int& key);
+	RBNode();
 	int bh() const;
 	int key() const;
-	int color() const;
+	NodeColor color() const;
 	RBNode* left() const;
 	RBNode* right() const;
 	RBNode* parent() const;
-	void setLeft(const RBNode* node);
-	void setRight(const RBNode* node);
-	void setParent(const RBNode* node);
+	void setLeft(RBNode* node);
+	void setRight(RBNode* node);
+	void setParent(RBNode* node);
+	void setKey(const int& key);
+	void setColor(const NodeColor& color);
+	
+	//operator Overload But now maybe not useful
+	//bool operator < (const RBNode& a) const
+	//{
+	//	return (*this).key()<a.key();
+	//}
+	//bool operator > (const RBNode& a) const  {return    a<(*this); }
+	//bool operator <= (const RBNode& a) const {return !((*this)>a); }
+	//bool operator >= (const RBNode& a) const {return !(a<(*this)); }
+	//bool operator == (const RBNode& a) const {return !(a>*this && *this>a); }
+	//bool operator !=(const RBNode& a) const  {return !(a==(*this));}
 };
 
 #endif // RBNODE_H
